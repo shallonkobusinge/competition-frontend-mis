5d22d4312ccdd0dad354e742fcb5b52a2cdbbb73
shallonkobusinge <kobshallon041@gmail.com>
2021-11-16 07:07:23 +0200

    [FIX] project boilerplate

--- src/App.js
+++ src/App.js
@@ -1,24 +1,65 @@
-import logo from './logo.svg';
 import './App.css';
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link,
+  Redirect,
+} from "react-router-dom";
+// import Login from './views/Login'
 
-function App() {
+// import UsersPage from './views/usersPage';
+
+function PrivateRoute({ children, ...rest }) {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Route
+      {...rest}
+      render={() => {
+        return sessionStorage.getItem("token") ? (
+          children
+        ) : (
+          <Redirect to="/login" />
+        );
+      }}
+    />
+  );
+}
+
+function PublicRoute({ children, ...rest }) {
+  if (!sessionStorage.getItem("token")) {
+    localStorage.removeItem("user");
+  }
+  return (
+    <Route
+      {...rest}
+      render={() => {
+        return !sessionStorage.getItem("token") ? (
+          children
+        ) : (
+          <Redirect to="/dashboard" />
+        );
+      }}
+    />
+  );
+}
+function App() {
+
+  return (
+    <>
+      <h2 >Hello World</h2>
+    </>
+    // <Router>
+    //   <Switch>
+    //     <PrivateRoute exact path="/users" >
+    //       <UsersPage />
+    //     </PrivateRoute>
+
+    //     <PublicRoute exact path="/login">
+    //       <Login />
+    //     </PublicRoute>
+    //   </Switch>
+    // </Router>
   );
 }
 
